11b1d9d392c9abf2b833fd9bcf70ed20b57e457b
Aisyah A Fitriani <aisyahafitriani05@gmail.com>
2023-11-07 11:54:05 +0700

    Project5 Navigation with Data

---
+++ app/src/main/java/com/example/project5_esjumbopage/data/DataSource.kt
@@ -0,0 +1,13 @@
+package com.example.project5_esjumbopage.data
+
+import com.example.project5_esjumbopage.R
+
+object DataSource {
+    val flavors = listOf(
+        R.string.original,
+        R.string.vanilla,
+        R.string.lemon,
+        R.string.milk,
+        R.string.lyche,
+    )
+}
